f45ced89c5b148812d98df938674cb954726d412
ww <wilson.wen.ky@gmail.com>
2023-01-28 11:08:09 +0100

    deploy-8

---
+++ assets/UserOrdersTab.da7653de.js
@@ -0,0 +1,1 @@
+import{a as e,j as s}from"./index.8865ada1.js";const a=()=>e("div",{className:"loader",style:{minHeight:"80vh"},children:[s("h3",{children:"My Orders"}),s("strong",{children:s("span",{className:"text-subtle",children:"You don't have any orders"})})]});export{a as default};
